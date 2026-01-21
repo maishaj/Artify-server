31c1590858a755f72d02b1642de373356d428f7e
maishaj <maishajannat388@gmail.com>
2026-01-21 21:26:06 +0600

    added users

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const cors=require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+
+//Mongodb
+
+const uri =
+  "mongodb+srv://ArtifyDBUser:vSbwqye54OpPWyYF@cluster0.fjenzci.mongodb.net/?appName=Cluster0";
 
 // Middleware
 
@@ -9,9 +15,40 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is running!..");
 });
 
 app.listen(port, () => {
   console.log(`Artify is running on port ${port}`);
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    //Creating Collection
+    const db = client.db("artify_db");
+    const usersCollection = db.collection("users");
+
+    //Adding Users to database
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
